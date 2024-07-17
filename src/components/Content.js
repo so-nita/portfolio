205748b527c1893e816e7a58c30e62bf7f3f218d
Back-End.07 <sonita.borin2@gmail.com>
2024-07-17 17:23:21 +0700

    deploy-application

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,15 @@
-
+import React from 'react';
 
 const Content = () => {
-
+    return (
+        <main className="content w-50 h-50 bg-light">
+        <div className="section h-100 w-100 d-flex justify-content-center" >
+            <p>
+                Passionate about technology and dedicated to creating innovative solutions. I aim to make a positive impact through software development.
+            </p>
+        </div>
+        </main>
+    );
 };
 
 export default Content;
