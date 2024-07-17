da2db64161ccf4e1b7cb32b994c6444716d1fb2b
Back-End.07 <sonita.borin2@gmail.com>
2024-07-17 16:16:26 +0700

    init-project

---
+++ src/components/VantaBackground.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import HALO from 'vanta/dist/vanta.halo.min';
+
+const VantaBackground = ({ children }) => {
+    const vantaRef = useRef(null);
+    
+};
+
+
+exports = VantaBackground;
